d9ea890335c752630cc1a73016d2dc7a7f1686f1
Guillermo Hernandez-Nieves <123615578+guillermo2001-afk@users.noreply.github.com>
2024-04-02 20:29:32 -0400

    Update server.js
    added the /public

--- server.js
+++ server.js
@@ -25,56 +25,57 @@ const server = http.createServer((req, res) => {
 
     switch (url) {
         case '/index':
-            serveStaticFile(res, '/index.html', 'text/html');
+            serveStaticFile(res, '/public/index.html', 'text/html');
             break;
         case '/about':
-            serveStaticFile(res, '/about.html', 'text/html');
+            serveStaticFile(res, '/public/about.html', 'text/html');
             break;
         case '/gallery':
-            serveStaticFile(res, '/gallery.html', 'text/html');
+            serveStaticFile(res, '/public/gallery.html', 'text/html');
             break;
         case '/pricing':
-            serveStaticFile(res, '/pricing.html', 'text/html');
+            serveStaticFile(res, '/public/pricing.html', 'text/html');
             break;    
         case '/stylesheet.css':
-            serveStaticFile(res, '/css/stylesheet.css', 'text/css');
+            serveStaticFile(res, '/public/css/stylesheet.css', 'text/css');
             break;
         case '/image1.webp':
-            serveStaticFile(res, '/images/image1.webp', 'image/webp');
+            serveStaticFile(res, '/public/images/image1.webp', 'image/webp');
             break;
         case '/image2.jpg':
-            serveStaticFile(res, '/images/image2.jpg', 'image/jpeg');
+            serveStaticFile(res, '/public/images/image2.jpg', 'image/jpeg');
             break;
         case '/image3.jpg':
-            serveStaticFile(res, '/images/image3.jpg', 'image/jpeg');
+            serveStaticFile(res, '/public/images/image3.jpg', 'image/jpeg');
             break;
         case '/image4.jpg':
-            serveStaticFile(res, '/images/image4.jpg', 'image/jpeg');
+            serveStaticFile(res, '/public/images/image4.jpg', 'image/jpeg');
             break;
         case '/script.js':
-            serveStaticFile(res, '/js/script.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/script.js', 'text/javascript');
             break;
         case '/galleryscript.js':
-            serveStaticFile(res, '/js/galleryscript.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/galleryscript.js', 'text/javascript');
             break;
         case '/pricingscript.js':
-            serveStaticFile(res, '/js/pricingscript.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/pricingscript.js', 'text/javascript');
             break;
         case '/newpricingscript.js':
-            serveStaticFile(res, '/js/newpricingscript.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/newpricingscript.js', 'text/javascript');
             break;
         case '/jquery_ajax_loader.js':
-            serveStaticFile(res, '/js/jquery_ajax_loader.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/jquery_ajax_loader.js', 'text/javascript');
             break;
         case '/jquery-3.7.1.min.js':
-            serveStaticFile(res, '/js/jquery-3.7.1.min.js', 'text/javascript');
+            serveStaticFile(res, '/public/js/jquery-3.7.1.min.js', 'text/javascript');
             break;   
         default:
-            serveStaticFile(res, '/404.html', 'text/html', 404);
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
     }
+    }
     
-});
+);
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
